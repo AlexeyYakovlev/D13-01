71fbf14c89d6742672814dd3daf841326757ef85
AlexeyYakovlev <yakovlev@radar-research.ru>
2013-07-29 21:09:07 +0400

    td

--- class/Coockies.php
+++ class/Coockies.php
@@ -3,11 +3,46 @@
 /**
  * @author Yakovlev
  * @version 1.0
- * Coockies (Factory method). 
+ * Coockies manager. 
  * Установка удаление кук.
  */
 class Coockies {
-    
+    /*
+     * @var $variable string
+     * @var $value string
+     * @var $period int
+     * Устанавливает куки на выбранный период.
+     */
+
+    static function install($variable, $value, $period = 1987200) {
+        setcookie($variable, $value, $period);
+    }
+
+    /*
+     * @var $variable string
+     * @var $value string
+     * @var $period int
+     * Удаляет куки.
+     */
+
+    static function uninstall($variable, $value, $period = 1987200) {
+        setcookie($variable, $value, $period);
+    }
+
+    /*
+     * @var $variable string
+     * @var $value string
+     * @var $period int
+     * Возвращает куки если они есть.
+     */
+
+    static function get($variable) {
+        if (isset($_COOKIE[$variable]))
+            return $_COOKIE[$variable];
+        else
+            return false;
+    }
+
 }
 
 ?>
