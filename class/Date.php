e3d06a4bdf6ee07c225968e1a09ab5aa4168df10
AlexeyYakovlev <yakovlev@radar-research.ru>
2013-07-30 19:08:24 +0400

    add class Date

---
+++ class/Date.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Date Static Class
+ *
+ * Дата, время и всё с этим связанное
+ *
+ * @package  Survey\Date
+ * @author   Yakovlev
+ * @version  1.0
+ */
+class Date {
+    // год
+
+    const YEAR = 31556926;
+    // месяц
+    const MONTH = 2629744;
+    // неделя
+    const WEEK = 604800;
+    // день
+    const DAY = 86400;
+    // час
+    const HOUR = 3600;
+    // минута
+    const MINUTE = 60;
+
+}
+
+?>
